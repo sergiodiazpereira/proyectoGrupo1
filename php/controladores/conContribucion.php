9a869d3a07fe8beaa3ab19d64723888a55359ca4
SkizoDev90 <franciscomanuelcarrillofernandez.guadalupe@alumnado.fundacionloyola.net>
2025-12-02 19:48:47 +0100

    Inicio_Server

---
+++ php/controladores/conContribucion.php
@@ -0,0 +1,18 @@
+<?php
+    require_once __DIR__.'/../config/rutas.php';
+    require_once __DIR__.'/../modelos/modContribucion.php';
+    class ConContribucion{
+
+        public function insertar(){
+        
+        }
+
+        public function modificar(){
+        
+        }
+
+        public function borrar(){
+        
+        }
+    }
+?>
---
+++ php/modelos/modAsociacion.php
@@ -0,0 +1,22 @@
+<?php
+    require_once __DIR__.'/../config/conexion.php';
+    
+    class ModAsociacion extends Conexion{
+
+        function __construct(){
+            parent::__construct();
+            
+        }
+        public function insertar(){
+        
+        }
+
+        public function modificar(){
+        
+        }
+
+        public function borrar(){
+        
+        }
+    }
+?>
