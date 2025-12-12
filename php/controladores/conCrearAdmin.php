5deed2ba51a8ca2d294a91481d10107c8a9109ed
spizarrop <santiagopizarropizarro.guadalupe@alumnado.fundacionloyola.net>
2025-12-12 22:16:19 +0100

    mas arreglos

--- php/controladores/conCrearAdmin.php
+++ php/controladores/conCrearAdmin.php
@@ -15,25 +15,40 @@
             $this->vista="admin/crearAdmin.php";
         }
 
+        private function functionValidar(){
+            if(empty(trim($_POST['nombreAdmin']))){return false;};
+            if(empty(trim($_POST['correoAdmin']))){return false;};
+            if(empty(trim($_POST['contraActual']))){return false;};
+            if(empty(trim($_POST['contraConfirmar']))){return false;};
+            if(trim($_POST['contraConfirmar'])!=trim($_POST['contraActual'])){return false;};
+            return true;
+        }
+
         public function guardarAdmin(): void
         {
+            
             $nombreAdmin = $_POST['nombreAdmin'];
             $correoAdmin = $_POST['correoAdmin'];
             $contraActual = $_POST['contraActual'];
             $contraConfirmar = $_POST['contraConfirmar'];
 
-            $adminRegistrado = $this->modelo->introducirAdmin($nombreAdmin, $correoAdmin, $contraActual, $contraConfirmar);
-
-            if ($adminRegistrado)
-            {
-                /* Es un mensaje que te redirige a la página de login.php, lo que hace el replace es sustituir la página en 
-                el historial en la que estabas por esa de login */ 
-                echo "<script>alert('Ya está registrado el administrador'); window.location.replace('index.php?c=Usuarios&m=cargarPagina');</script>";
-            }
-            else
-            {
+            if($this->functionValidar()){
+                $adminRegistrado = $this->modelo->introducirAdmin($nombreAdmin, $correoAdmin, $contraActual, $contraConfirmar);
+            
+                if ($adminRegistrado == true)
+                {
+                    /* Es un mensaje que te redirige a la página de login.php, lo que hace el replace es sustituir la página en 
+                    el historial en la que estabas por esa de login */ 
+                    echo "<script>alert('Ya está registrado el administrador'); window.location.replace('index.php?c=Usuarios&m=cargarPagina');</script>";
+                }else{
+                    echo "<script>alert('Las contraseñas no coinciden o el correo ya está en uso'); window.location.replace('index.php?c=CrearAdmin&m=cargarPagina');</script>";
+                }
+                
+            }else{
                 echo "<script>alert('Las contraseñas no coinciden o el correo ya está en uso'); window.location.replace('index.php?c=CrearAdmin&m=cargarPagina');</script>";
             }
+
+            
         }
     }
 
--- php/vistas/usuario/login.php
+++ php/vistas/usuario/login.php
@@ -1,3 +1,7 @@
+<?php
+    //require_once __DIR__.'/../../controladores/conLogin.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
