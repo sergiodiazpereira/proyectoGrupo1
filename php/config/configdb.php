07b38c3e10eb11e55d6ac5e2baaf006727f6ee08
SkizoDev90 <franciscomanuelcarrillofernandez.guadalupe@alumnado.fundacionloyola.net>
2025-12-14 16:38:14 +0100

    final

--- php/config/configdb.php
+++ php/config/configdb.php
@@ -1,6 +1,6 @@
 <?php
-    define('SERVIDOR','20.daw.esvirgua.com');
-    define('USUARIO','daw_userbd20');
-    define('PASSWORD','o-G-hHoh9IS&t6rt');
-    define('BBDD','daw_20_BD1');
+    define('SERVIDOR','localhost');
+    define('USUARIO','root');
+    define('PASSWORD','');
+    define('BBDD','asociaciondle');
 ?>
