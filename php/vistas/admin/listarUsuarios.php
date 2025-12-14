ab419c30b7922ed33c9adbb5e312ee8a896767d1
Sergio <sergiodiazpereira.guadalupe@alumnado.fundacionloyola.net>
2025-12-14 16:41:03 +0100

    cambiar mayus imagenes

--- php/vistas/admin/listarUsuarios.php
+++ php/vistas/admin/listarUsuarios.php
@@ -67,7 +67,7 @@
             </ul>
             <ul class="ul-inferior">
                 <li>
-                    <a href="index.php?c=Usuario&m=cargarPagina">
+                    <a href="index.php?c=CambioAdmin&m=cargarPagina">
                         <button>
                             <i class="fa-solid fa-key"></i>
                             <span>Cambiar contraseña</span>
